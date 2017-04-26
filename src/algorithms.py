e97b3dc80d8b7670c7775b99a0d7f1868e41cc93
yuanj <yuanj@bupt.edu.cn>
2017-04-26 22:02:37 +0800

    ADD: k-means Fix: bug in random

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,5 +1,10 @@
+import logging
 import random
+from datetime import datetime
 from typing import List
+
+import numpy as np
+import scipy.cluster.vq as vq
 
 from base_station import BaseStation
 from edge_server import EdgeServer
@@ -24,8 +29,6 @@ class ServerPlacement(object):
     def objective_latency(self):
         assert self.edge_servers
 
-        pass
-
     def objective_workload(self):
         assert self.edge_servers
 
@@ -37,7 +40,23 @@ class MIPServerPlacement(ServerPlacement):
 
 class KMeansServerPlacement(ServerPlacement):
     def place_server(self, edge_server_num):
-        pass
+        logging.info("{0}:Start running k-means".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        # data: ndarray
+        base_stations = self.base_stations
+        coordinates = list(map(lambda x: (x.latitude, x.longitude), base_stations))
+        data = np.array(coordinates)
+        k = edge_server_num
+
+        # k-means
+        centroid, label = vq.kmeans2(data, k, iter=100, minit='points')
+
+        # process result
+        edge_servers = [EdgeServer(i, row[0], row[1]) for i, row in enumerate(centroid)]
+        for bs, es in enumerate(label):
+            edge_servers[es].assigned_base_stations.append(base_stations[bs])
+
+        self.edge_servers = edge_servers
+        logging.info("{0}:End running  k-means".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 
 class TopKServerPlacement(ServerPlacement):
@@ -45,6 +64,7 @@ class TopKServerPlacement(ServerPlacement):
         """
         Top-K approach
         """
+        logging.info("{0}:Start running Top-k".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
         sorted_base_stations = sorted(self.base_stations, key=lambda x: x.workload, reverse=True)
         edge_servers = [EdgeServer(i, item.latitude, item.longitude, item.id) for i, item in
                         enumerate(sorted_base_stations[:edge_server_num])]
@@ -56,8 +76,9 @@ class TopKServerPlacement(ServerPlacement):
                 if tmp < min_distance:
                     min_distance = tmp
                     closest_edge_server = edge_server
-            closest_edge_server.assgined_base_stations.append(base_station)
+            closest_edge_server.assigned_base_stations.append(base_station)
         self.edge_servers = edge_servers
+        logging.info("{0}:End running Top-k".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
 
 
 class RandomServerPlacement(ServerPlacement):
@@ -65,10 +86,12 @@ class RandomServerPlacement(ServerPlacement):
         """
         Random approach
         """
+        base_stations = self.base_stations
+        logging.info("{0}:Start running Random".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
         random_base_stations = random.sample(self.base_stations, edge_server_num)
         edge_servers = [EdgeServer(i, item.latitude, item.longitude, item.id) for i, item in
                         enumerate(random_base_stations)]
-        for i, base_station in enumerate(random_base_stations):
+        for i, base_station in enumerate(base_stations):
             closest_edge_server = None
             min_distance = 1e10
             for j, edge_server in enumerate(edge_servers):
@@ -76,5 +99,6 @@ class RandomServerPlacement(ServerPlacement):
                 if tmp < min_distance:
                     min_distance = tmp
                     closest_edge_server = edge_server
-            closest_edge_server.assgined_base_stations.append(base_station)
+            closest_edge_server.assigned_base_stations.append(base_station)
         self.edge_servers = edge_servers
+        logging.info("{0}:End running Random".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
--- src/debug.py
+++ src/debug.py
@@ -1,5 +1,6 @@
 import logging
 
+from algorithms import *
 from utils import Utils
 
 if __name__ == '__main__':
@@ -8,4 +9,12 @@ if __name__ == '__main__':
     tmp = Utils.base_station_reader('data/基站经纬度.csv')
     bs = Utils.user_info_reader('data/上网信息输出表（日表）6月15号之后.csv', tmp)
     dist = Utils.distance_between_stations(tmp)
+    kmeans_placer = KMeansServerPlacement(bs, dist)
+    kmeans_placer.place_server(100)
+    top_k_placer = TopKServerPlacement(bs, dist)
+    top_k_placer.place_server(100)
+    random_placer = RandomServerPlacement(bs, dist)
+    random_placer.place_server(100)
+    for server in kmeans_placer.edge_servers:
+        print(len(server.assigned_base_stations))
     pass
