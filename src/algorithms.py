5cda987dab40baf93b54a30f73cf6c56cbfb1820
yuanj <yuanj@bupt.edu.cn>
2017-04-29 15:46:00 +0800

    Almost finish _(:зゝ∠)_

--- src/algorithms.py
+++ src/algorithms.py
@@ -1,7 +1,8 @@
 import logging
+import math
 import random
 from datetime import datetime
-from typing import List
+from typing import List, Iterable
 
 import cplex
 import numpy as np
@@ -63,31 +64,166 @@ class ServerPlacement(object):
 
 
 class MIPServerPlacement(ServerPlacement):
-    def preprocess_problem(self, k):
-        # 每个基站，找出距离它最近的N/K个基站
-        d = np.array(self.distances)
-        cap = int(len(self.base_stations) / k)
-        assign = []
-        for i, row in enumerate(d):
-            indices = row.argpartition(cap)[:cap]
-            assign.append(indices.tolist())
-            logging.debug("Found nearest {0} base stations of base station {1}".format(cap, i))
+    """
+    MIP approach
+    """
 
-        pass
-
-    def setup_problem(self, c):
-        c.objective.set_sense(c.objective.sense.minimize)
-
-        c.linear_constraints.add()
+    def __init__(self, base_stations: List[BaseStation], distances: List[List[float]]):
+        super().__init__(base_stations, distances)
+        self.n = 0
+        self.k = 0
+        self.weights = None
+        self.belongs = None
+        self.assgin = None
+        self.placement_vars = None
+        self.assigned_vars = None
 
     def place_server(self, base_station_num, edge_server_num):
+        logging.info("{0}:Start running MIP with N={1}, K={2}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+                     base_station_num, edge_server_num)
+        self.n = base_station_num
+        self.k = edge_server_num
 
-        self.preprocess_problem(edge_server_num)
+        self.preprocess_problem()
 
-        c = cplex.Cplex
+        c = cplex.Cplex()
 
         self.setup_problem(c)
+
+        c.solve()
+
+        assert self.placement_vars
+        assert self.assigned_vars
+        solution = c.solution
+        if solution.is_primal_feasible():
+            print("Solution value = ", solution.get_objective_value())
+            solution_vars = [solution.get_values(var) for var in self.placement_vars]
+            assigned_vars = [solution.get_values(var) for var in self.assigned_vars]
+            places = [i for i, x in enumerate(solution_vars) if x == 1]
+            print(places)
+            print(assigned_vars)
+            self.process_result(places)
+        else:
+            print("No solution available")
+        logging.info("{0}:End running MIP".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+
+    def preprocess_problem(self):
+        base_stations = self.base_stations[:self.n]
+        # 每个基站，找出距离它最近的N/K个基站
+        d = np.array([row[:self.n] for row in self.distances[:self.n]])
+        cap = int(len(base_stations) / self.k)
+        assign = []
+        max_distances = []  # 距离
+        for i, row in enumerate(d):
+            indices = row.argpartition(cap)[:cap]
+            assign.append(indices)
+            t = row[indices]
+            max_distances.append(row[indices].max())
+            logging.debug("Found nearest {0} base stations of base station {1}".format(cap, i))
+
+        # 负载
+        avg_workload = sum((item.workload for item in base_stations)) / self.k
+        workload_diff = []
+        for row in assign:
+            assigned_stations = (base_stations[item] for item in row)
+            workload = sum((item.workload for item in assigned_stations))
+            expr = math.pow(workload - avg_workload, 2)
+            workload_diff.append(expr)
+
+        # 归一化
+        normalized_max_distances = MIPServerPlacement._normalize(max_distances)
+        normalized_workload_diff = MIPServerPlacement._normalize(workload_diff)
+
+        belongs = [[] for i in range(self.n)]  # belongs: 表示一个基站要被照顾到，可以在那些地方部署边缘服务器
+        for i, row in enumerate(assign):
+            for bs in row:
+                belongs[bs].append(i)
+
+        alpha = 0.2
+        self.weights = [alpha * normalized_max_distances[i] + (1 - alpha) * normalized_workload_diff[i] for i in
+                        range(self.n)]
+        self.belongs = belongs
+        self.assign = assign
+
         pass
+
+    def setup_problem(self, c: cplex.Cplex):
+        assert self.weights
+        assert self.belongs
+
+        c.objective.set_sense(c.objective.sense.minimize)
+
+        # placement variables: placement[i] = 1 if a edge server is placed with base station i
+        placement_vars = []
+        for i in range(self.n):
+            varname = "place_{0}".format(i)
+            placement_vars.append(varname)
+        c.variables.add(obj=self.weights, names=placement_vars, lb=[0] * len(placement_vars),
+                        ub=[1] * len(placement_vars),
+                        types=[c.variables.type.binary] * len(placement_vars))
+
+        # assigned variables: assigned[i] = 1 if base station i has been assigned to at least one edge server
+        assigned_vars = []
+        for i in range(self.n):
+            varname = "assigned_{0}".format(i)
+            assigned_vars.append(varname)
+        c.variables.add(names=assigned_vars, lb=[0] * len(assigned_vars), ub=[1] * len(assigned_vars),
+                        types=[c.variables.type.binary] * len(assigned_vars))
+
+        # constraint: total number of edge servers should be K
+        c.linear_constraints.add(lin_expr=[cplex.SparsePair(placement_vars, [1 for i in range(self.n)])],
+                                 senses=['E'], rhs=[self.k])
+
+        # constraint: assigned 表示是否为基站分配了边缘服务器
+        # assigned[i] >= all place[j] for j in belongs[i]
+        for bsid, esids in enumerate(self.belongs):
+            varnames = []
+            coefficients = []
+            assigned_varname = "assigned_{0}".format(bsid)
+            for esid in esids:
+                place_varname = "place_{0}".format(esid)
+                varnames.append(place_varname)
+                coefficients.append(1)
+                c.linear_constraints.add(lin_expr=[cplex.SparsePair([assigned_varname, place_varname], [1, -1])],
+                                         senses=['G'], rhs=[0])
+            varnames.append(assigned_varname)
+            coefficients.append(-1)
+            c.linear_constraints.add(lin_expr=[cplex.SparsePair(varnames, coefficients)],
+                                     senses=['G'], rhs=[0])
+
+        # constraint: 被分配了边缘服务器的基站的总数
+        acceptable = int(self.n * 0.9)
+        c.linear_constraints.add(lin_expr=[cplex.SparsePair(assigned_vars, [1 for i in range(self.n)])],
+                                 senses=['G'], rhs=[acceptable])
+
+        self.assigned_vars = assigned_vars
+        self.placement_vars = placement_vars
+
+    def process_result(self, solution):
+        """
+        :param solution: a list containing all id of base stations selected to put an edge server with it
+        :return: 
+        """
+        base_stations = self.base_stations[:self.n]
+        edge_servers = [EdgeServer(i, base_stations[x].latitude, base_stations[x].longitude, base_stations[x].id)
+                        for i, x in enumerate(solution)]
+        for i, base_station in enumerate(base_stations):
+            closest_edge_server = None
+            min_distance = 1e10
+            for j, edge_server in enumerate(edge_servers):
+                tmp = self._distance_edge_server_base_station(edge_server, base_station)
+                if tmp < min_distance:
+                    min_distance = tmp
+                    closest_edge_server = edge_server
+            closest_edge_server.assigned_base_stations.append(base_station)
+            closest_edge_server.workload += base_station.workload
+        self.edge_servers = edge_servers
+
+    @staticmethod
+    def _normalize(l: Iterable):
+        minimum = min(l)
+        delta = max(l) - minimum
+        return [(i - minimum) / delta for i in l]
 
 
 class KMeansServerPlacement(ServerPlacement):
@@ -96,7 +232,8 @@ class KMeansServerPlacement(ServerPlacement):
     """
 
     def place_server(self, base_station_num, edge_server_num):
-        logging.info("{0}:Start running k-means".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        logging.info("{0}:Start running k-means with N={1}, K={2}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+                     base_station_num, edge_server_num)
         # init data as ndarray
         base_stations = self.base_stations[:base_station_num]
         coordinates = list(map(lambda x: (x.latitude, x.longitude), base_stations))
@@ -122,7 +259,8 @@ class TopKServerPlacement(ServerPlacement):
     """
 
     def place_server(self, base_station_num, edge_server_num):
-        logging.info("{0}:Start running Top-k".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        logging.info("{0}:Start running Top-k with N={1}, K={2}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+                     base_station_num, edge_server_num)
         base_stations = self.base_stations[:base_station_num]
         sorted_base_stations = sorted(base_stations, key=lambda x: x.workload, reverse=True)
         edge_servers = [EdgeServer(i, item.latitude, item.longitude, item.id) for i, item in
@@ -148,7 +286,8 @@ class RandomServerPlacement(ServerPlacement):
 
     def place_server(self, base_station_num, edge_server_num):
         base_stations = self.base_stations[:base_station_num]
-        logging.info("{0}:Start running Random".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')))
+        logging.info("{0}:Start running Random with N={1}, K={2}".format(datetime.now().strftime('%Y-%m-%d %H:%M:%S')),
+                     base_station_num, edge_server_num)
         random_base_stations = random.sample(self.base_stations, edge_server_num)
         edge_servers = [EdgeServer(i, item.latitude, item.longitude, item.id) for i, item in
                         enumerate(random_base_stations)]
---
+++ src/run_and_print.py
@@ -0,0 +1,67 @@
+from typing import Dict
+
+from algorithms import *
+from utils import *
+
+
+def run_problem(problem: ServerPlacement, n, k):
+    problem.place_server(n, k)
+    return problem.objective_latency(), problem.objective_workload()
+
+
+def run_with_parameters(problems: Dict[str, ServerPlacement], n, k):
+    results = {}
+    # results['MIP'] = run_problem(problems['MIP'], n, k)
+    results['Top-K'] = run_problem(problems['Top-K'], n, k)
+
+    sum_a = 0
+    sum_b = 0
+    for t in range(10):
+        res = run_problem(problems['K-means'], n, k)
+        sum_a += res[0]
+        sum_b += res[1]
+    results['K-means'] = (sum_a / 10, sum_b / 10,)
+
+    sum_a = 0
+    sum_b = 0
+    for t in range(10):
+        res = run_problem(problems['Random'], n, k)
+        sum_a += res[0]
+        sum_b += res[1]
+    results['Random'] = (sum_a / 10, sum_b / 10,)
+
+    return results
+
+
+def run(data: DataUtils):
+    problems = {}
+    # problems['MIP'] = MIPServerPlacement(data.base_stations, data.distances)
+    problems['K-means'] = KMeansServerPlacement(data.base_stations, data.distances)
+    problems['Top-K'] = TopKServerPlacement(data.base_stations, data.distances)
+    problems['Random'] = RandomServerPlacement(data.base_stations, data.distances)
+    with open('data/results.txt', 'w') as file:
+        # 第一个图
+        for n in range(300, 3300, 300):
+            k = int(n / 10)
+            print("N={0}, K={1}".format(n, k), file=file)
+            results = run_with_parameters(problems, n, k)
+            for key, value in results.items():
+                print(key, "平均距离(km)={0}, 负载标准差={1}".format(value[0], value[1]), file=file)
+                file.flush()
+
+        print("======================================================", file=file)
+
+        n = 3000
+        for k in range(100, 600, 100):
+            print("N={0}, K={1}".format(n, k), file=file)
+            results = run_with_parameters(problems, n, k)
+            for key, value in results.items():
+                print(key, "平均距离(km)={0}, 负载标准差={1}".format(value[0], value[1]), file=file)
+                file.flush()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+
+    data = DataUtils('data/基站经纬度.csv', 'data/上网信息输出表（日表）6月15号之后.csv')
+    run(data)
